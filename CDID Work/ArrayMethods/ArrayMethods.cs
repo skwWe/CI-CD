a7d332ccebd6f7ed78f57ceec88f3a5cbd64bac2
unknown <@ngknn.local>
2025-03-22 18:12:53 +0300

    First commit

---
+++ CDID Work/ArrayMethods/ArrayMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ArrayMethods
+{
+    public class ArrayClass
+    {
+        public int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        public int[] ArraySort()
+        {
+            Array.Sort(array);
+            //Console.WriteLine("");
+            foreach (var value in array)
+                Console.WriteLine(value + " ");
+            return array;
+        }
+
+        public void ArrayFilter()
+        {
+            var DivisibleByThree = from i in array
+                                   where i % 3 == 1
+                                   select i;
+        }
+    }
+}
---
+++ CDID Work/UnitTests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayMethods;
+using System;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ArrayTests
+    {
+        public int[] array;
+        public ArrayClass _arraytest;
+        [TestInitialize]
+        public void Setup()
+        {
+            array = new int[] { 9, 2, 7, 4, 5, 6, 1, 8, 3 };
+            _arraytest = new ArrayClass();
+            _arraytest.array = (int[])array.Clone();
+        }
+        [TestMethod]
+        public void ArraySort_ShouldTestSort()
+        {
+            int[] expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            _arraytest.ArraySort();
+            CollectionAssert.AreEqual(expected, _arraytest.array);
+        }
+
+        [TestMethod]
+        public void ArrayFilter_ShouldFilt()
+        {
+            int[] expected = new int[] { 3, 6, 9 };
+            var result = _arraytest.ArrayFilter();
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+    }
+}
